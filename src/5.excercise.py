8d49607e671c32e87448687f138910d73868b66e
Joseph John <joseph.john@anu.edu.au>
2024-02-26 17:02:01 +1000

    Excercise 1

---
+++ src/5.excercise.py
@@ -0,0 +1,28 @@
+import numpy as np
+
+N = 400000
+
+def create_array(n):
+    arr = np.arange(n)
+    return arr
+
+def numpy_multiple(arr1, arr2):
+    result = np.multiply(arr1, arr2)
+
+def slow_multiply(arr1, arr2):
+    result = []
+
+    for i in range (0, N):
+        result.append(arr1[i] * arr2[i])
+
+
+def main():
+    arr1 = create_array(N)
+    arr2 = create_array(N)
+
+    numpy_multiple(arr1, arr2)
+    slow_multiply(arr1, arr2)
+
+
+if __name__ == '__main__':
+    print('Hint: Insert profiling code here')
